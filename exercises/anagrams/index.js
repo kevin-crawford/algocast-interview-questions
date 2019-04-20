9ab96de05c590d074b2fcda8fc057ccfc7fd5fc0
Kevin Crawford <kcdouglas92@gmail.com>
2019-04-20 12:21:57 -0700

    completed anagrams exercise

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -7,7 +7,47 @@
 //   anagrams('rail safety', 'fairy tales') --> True
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
+function anagrams(stringA, stringB) {
+  return cleanString(stringA) === cleanString(stringB);
+}
 
-function anagrams(stringA, stringB) {}
+function cleanString(str) {
+  //remove whitespace and non alphanumerics, lowercase string,
+  // turn string into array, sort array, join array into string
+  return str
+    .replace(/[^\w]/g)
+    .toLowerCase()
+    .split("")
+    .sort()
+    .join("");
+}
 
 module.exports = anagrams;
+
+// function anagrams(stringA, stringB) {
+//   let charMapA = charMap(stringA);
+//   let charMapB = charMap(stringB);
+
+//   // check lengths of maps, if not equal, return false
+//   if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+//     return false;
+//   }
+//   // iterate over a charmapA, if the character in mapA is not equal to character in mapB, return false
+//   for (let char in charMapA) {
+//     if (charMapA[char] !== charMapB[char]) {
+//       return false;
+//     }
+//   }
+// 	// if logic passes, return true;
+//   return true;
+// }
+
+// function charMap(str) {
+//   let map = {};
+//   // remove all whitespace and non alphanumeric characters, create map
+//   for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+//     map[char] = map[char] + 1 || 1;
+//   }
+
+//   return map;
+// }
